c6e19641ddcd02f2707438bd187d4e287a5af618
hellogaurav21 <59482605+hellogaurav21@users.noreply.github.com>
2026-02-16 21:57:38 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,15 +2,22 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.36.0"
-    }
-  }
+      version = "4.50.0"
+     }
+ }
 }
  provider "azurerm" {
-    features { }
-
+    resource_provider_registrations = "none"
+    features {
+            }
+client_id = "e087a58a-9022-4140-97eb-91b4dbf23eea"
+tenant_id = "f80ef77f-8297-4c19-be36-994c9d9a89b3"
+subscription_id ="12740c98-9a2c-4549-abd6-3c7478277763"
+client_secret = "uJC8Q~LpvX9cQcbs.4hWiFi0NAxfVQ1UraYb0cMy"
  }
 
-resource "azurerm_resource_group" "rg2-gaurav" {
-  name = "test2_gaurav2"
+ # 1. Define the Resource Group
+resource "azurerm_resource_group" "avd2_rg" {
+  name     = "rg2-avd2-demo2"
   location = "East US"
+}
